bd105162437ecd0b59002c7fcda630c6ae79236d
Brayan-Cassaleth <b.cassalethr@gmail.com>
2025-09-08 17:14:50 -0500

    Agregamos formulario y un scroll cuando se carga la pagina

--- funciones.js
+++ funciones.js
@@ -1,9 +1,9 @@
-const btn = document.getElementById("ingresar");
-const formulario = document.getElementById("login");
-
-btn.addEventListener("click", function() {
-// Alternar entre oculto y visible
-if (formulario.style.display === "none" || formulario.style.display === "") {
-    formulario.style.display = "block";
-}
-});
+ScrollReveal().reveal('.banner', {delay: 200});
+ScrollReveal().reveal('#llamado-accion', {delay: 500});
+ScrollReveal().reveal('.portafolio', {delay: 500});
+ScrollReveal().reveal('#servicios', {delay: 500});
+ScrollReveal().reveal('#clientes', {delay: 500});
+ScrollReveal().reveal('#formulario', {delay: 500});
+ScrollReveal().reveal('#texto_formulario', {delay: 500});
+ScrollReveal().reveal('#agndar_cita', {delay: 500});
+ScrollReveal().reveal('#pie-pagina', {delay: 500});
