626634dce403ed0822637813daf97ffb4ba5f80f
Brayan-Cassaleth <b.cassalethr@gmail.com>
2025-09-14 22:19:04 -0500

    Le configuramos el boton al formulario y modificamos las imagenes de los clientes

--- funciones.js
+++ funciones.js
@@ -7,3 +7,34 @@ ScrollReveal().reveal('#formulario', {delay: 500});
 ScrollReveal().reveal('#texto_formulario', {delay: 500});
 ScrollReveal().reveal('#agndar_cita', {delay: 500});
 ScrollReveal().reveal('#pie-pagina', {delay: 500});
+
+const formulario = document.getElementById("agendar_cita");
+const inputs = formulario.querySelectorAll("input, textarea");
+const boton = document.getElementById("boton");
+
+function validarFormulario() {
+    // Verifica que todos los inputs tengan valor
+    let vacios = Array.from(inputs).some(input => input.value.trim() === "");
+    boton.disabled = vacios; // Si hay vacío → bloquea
+}
+
+// Detectar cambios en los inputs
+inputs.forEach(input => {
+    input.addEventListener("input", validarFormulario);
+});
+
+formulario.addEventListener("submit", function(e) {
+    e.preventDefault(); // Evita recargar la página
+    Swal.fire({
+        title: "Correo enviado con exito!",
+        icon: "success",
+        draggable: true
+    });
+
+      // Limpiar todos los campos
+      formulario.reset();
+
+      // Volver a bloquear el botón
+      boton.disabled = true;
+    });
+
