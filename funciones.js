5745a31cef89cfed8daab7893bce14d4caffe5e8
Brayan-Cassaleth <b.cassalethr@gmail.com>
2025-09-15 23:10:22 -0500

    Modificamos los tamaños para las diferentes pantallas

--- funciones.js
+++ funciones.js
@@ -1,12 +1,12 @@
 ScrollReveal().reveal('.banner', {delay: 200});
-ScrollReveal().reveal('#llamado-accion', {delay: 500});
-ScrollReveal().reveal('.portafolio', {delay: 500});
-ScrollReveal().reveal('#servicios', {delay: 500});
-ScrollReveal().reveal('#clientes', {delay: 500});
-ScrollReveal().reveal('#formulario', {delay: 500});
-ScrollReveal().reveal('#texto_formulario', {delay: 500});
-ScrollReveal().reveal('#agndar_cita', {delay: 500});
-ScrollReveal().reveal('#pie-pagina', {delay: 500});
+ScrollReveal().reveal('#llamado-accion', {delay: 300});
+ScrollReveal().reveal('.portafolio', {delay: 300});
+ScrollReveal().reveal('#servicios', {delay: 300});
+ScrollReveal().reveal('#clientes', {delay: 300});
+ScrollReveal().reveal('#formulario', {delay: 300});
+ScrollReveal().reveal('#texto_formulario', {delay: 300});
+ScrollReveal().reveal('#agndar_cita', {delay: 300});
+ScrollReveal().reveal('#pie-pagina', {delay: 300});
 
 const formulario = document.getElementById("agendar_cita");
 const inputs = formulario.querySelectorAll("input, textarea");
